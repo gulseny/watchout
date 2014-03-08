5305679509aef09d0f47929a714e0150d27c4106
Hack Reactor Students <gsenol@gmail.com>
2014-03-07 20:49:02 -0800

    player circle is draggable

--- watchout.js
+++ watchout.js
@@ -1,14 +1,10 @@
-// start slingin' some d3 here.
-
-// Create a board
-// d3.select('body').append('svg').attr({width: window.innerWidth, height: window.innerHeight});
-// var circle = {cx:"10"; cy:"10"; r:"10"; fill:"black"}
 var svg = d3.select('body').append('svg');
-var w = window.innerWidth, h = window.innerHeight;
+var w = window.innerWidth/2, h = window.innerHeight/2;
 svg.attr({width: w, height: h});
 
 // Create Circle Constructor
 var Circle = function(){
+  this.class = 'enemy';
   this.r = 10;
   this.cx = Math.random() * w;
   this.cy = Math.random() * h;
@@ -20,6 +16,7 @@ var appendCircle = function(array){
   .data(array)
   .enter()
   .append('circle')
+  .attr('class', function(d){return d.class})
   .attr('r', function(d){return d.r})
   .attr('cx', function(d){return d.cx})
   .attr('cy', function(d){return d.cy})
@@ -29,17 +26,47 @@ var nCircles = function(n){
   var arrayCircle = [];
   for(var i = 0; i < n; i++){
     arrayCircle.push(new Circle());
-    appendCircle(arrayCircle);
   }
+  appendCircle(arrayCircle);
 };
 
-nCircles(30);
+nCircles(10);
+var oneCircle = d3.select('circle');
+oneCircle.attr('class','player')
+.attr('cx', w/2)
+.attr('cy', h/2)
+.call(d3.behavior.drag().on("drag", move));
+// .call(d3.behavior.drag()
+// .on("player", move));
 
 // setInterval(callback, wait);
 setInterval(function(){
-  var circles = d3.selectAll('circle');
-  circles.transition()
+  var circles = d3.selectAll('.enemy');
+  circles
+    .transition()
     .duration(1000)
     .attr('cx', function(d){return Math.random()* w})
     .attr('cy', function(d){return Math.random()* h})
+}, 1000);
+
+function move(){
+    this.parentNode.appendChild(this);
+    var player = d3.select(this);
+    player
+        .attr("cx", function(){return d3.event.dx + parseInt(player.attr("cx"))})
+        .attr("cy", function(){return d3.event.dy + parseInt(player.attr("cy"))});
+};
+
+var getCoords = function(className){
+  var player = d3.selectAll('circle')
+    .attr("cx", function(d){ console.log('cx', d.cx) })
+    .attr("cy", function(d){ console.log('cy', d.cy) });
+
+  // console.log('cx:' , player.attr('cx'));
+  // console.log('cy:' , player.attr('cy'));
+  // .event.dx;
+  // .event.dx
+}
+setInterval(function(){
+  getCoords();
 }, 1000);
